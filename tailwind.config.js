29eb8806763e16d457eb8b86e83907cfc355fb10
karencold <kvartapetov@gmail.com>
2020-07-27 11:38:24 +0300

    dev env init; tailwind; sass;

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    // enabled: true,
+    content: ['./src/**/*.js', './src/**/*.jsx', 'public/**/*.html'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
